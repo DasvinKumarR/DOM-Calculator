9f5f03e22f5800da0e695b8083c523854fc5c037
DasvinKumarR <dasvinkumar2404@gmail.com>
2024-06-09 14:42:02 +0530

    comments added in code

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// Value initalized to store input
 let currentVal = "";
+// Total result sum variable  initialization
 let sum = 0
-document.getElementById("result").value = sum
+// Added event listener to get the input from keyboard only numbers
 document.addEventListener('keydown', function (e) {
     let input = e.key;
     if (!isNaN(parseInt(input))) {
@@ -10,22 +12,22 @@ document.addEventListener('keydown', function (e) {
         alert("Only numbers are allowed")
     }
 })
-
+// Created function to delete the numbers
 function del() {
     currentVal = currentVal.slice(0, -1)
     document.getElementById('result').value = currentVal
 }
-
+// Created function to get the element values
 function getValue(e) {
     currentVal += e
     document.getElementById('result').value = currentVal
 }
-
+// Created funcrion to clear the input box
 function clr() {
     currentVal = ''
     document.getElementById('result').value = currentVal
 }
-
+// Created function to calculate the numbers
 function cal() {
     try{
         sum = eval(currentVal)
